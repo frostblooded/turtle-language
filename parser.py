37b4817735c0b00d547fb4c338a829dffdfac200
frostblooded <frostblooded@yahoo.com>
2017-05-27 19:50:17 +0300

    Start making the turtle language parser

---
+++ parser.py
@@ -0,0 +1,4 @@
+class Parser:
+    @staticmethod
+    def parse_file(file):
+        print(file)
---
+++ tl.py
@@ -0,0 +1,12 @@
+import sys
+
+from parser import Parser
+
+if __name__ == "__main__":
+    arguments_count = len(sys.argv) - 1
+
+    if arguments_count > 1:
+        print('Please only pass a single file')
+
+    file_arg = sys.argv[1]
+    Parser.parse_file(file_arg)
